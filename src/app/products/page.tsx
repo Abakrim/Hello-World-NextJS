5512c780e47b6f5039771b325cea5957f64889d1
Abakrim <abakrim.dev@gmail.com>
2025-03-09 16:34:33 +0000

    example of dynamic routes

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+const Products = () => {
+    return(
+        <div>
+            <h1>Products List</h1>
+            <ul>
+                <li>product 1</li>
+                <li>product 2</li>
+                <li>product 3</li>
+            </ul>
+        </div>
+    );
+}
+export default Products;
