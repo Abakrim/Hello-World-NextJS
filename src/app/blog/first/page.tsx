5a45174b422e936157d401321c51fa336c22761f
Abakrim <abakrim.dev@gmail.com>
2025-03-09 16:13:36 +0000

    now I tested the nested basic routes

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,4 @@
+const First = () => {
+    return <h1>this is my first blog</h1>
+}
+export default First;
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,4 @@
+const Second = () => {
+    return <h1>This is my second blog!</h1>
+}
+export default Second
